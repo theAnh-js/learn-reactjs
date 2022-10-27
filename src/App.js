e61a3177a99b2881f7fdac9b227c6b60d073381f
theAnh-js <hoanbao1999@gmail.com>
2022-10-27 22:15:49 +0900

    update title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="app">Learn ReactJS - Easy Frontend</div>;
+  return <div className="app">Learn ReactJS - Easy Frontend.</div>;
 }
 
 export default App;
